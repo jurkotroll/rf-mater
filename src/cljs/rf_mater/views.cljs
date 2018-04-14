e82278cb66667aa203310f934fc80aab67eece9b
jurkotroll <jurko.bossetroll@gmail.com>
2018-04-14 03:50:03 +0200

    first thru commit; first page: cards function, views

--- src/cljs/rf_mater/views.cljs
+++ src/cljs/rf_mater/views.cljs
@@ -6,33 +6,69 @@
     				[cljs-react-material-ui.reagent :as ui]
     				[cljs-react-material-ui.icons :as ic]
     				[reagent.core :as r]
+            [garden.core :refer [css]]
             ))
 
 ;; home
 ; Example with various components
+(defn app-bar []
+  (let []
+    [ui/app-bar {:title "Razem Na Stok"
+								 :showMenuIconButton false
+								 :titleStyle {:color (color :lime200)
+															:textAlign :center
+									}}]))
+
+(defn jumbotron []
+  (let []
+    [ui/paper {:style {:height "200px"
+											 :width "auto"
+											 ;:max-width "40%"
+											 :margin "10px 0px"
+											 :textAlign "center"
+											 :display "flex"
+											}
+							 :zDepth "1"}]))
 
 
 
+(defn new-card [{:keys [title subtitle]}]
+  (let [
+        ;title (:title titles) subtitle (:subtitle titles)
+        ]
+    [ui/card {:style {:height "auto"
+                      :width "auto"
+                      :margin "10px 0px"}}
+     [ui/card-header {:title title
+                      :subtitle subtitle}]]))
+
+(defn car-card []
+  (let []
+    (new-card {:title "Nazwa"
+               :subtitle "numer porządkowy auta"})))
+
+(defn new-passanger-card []
+  (let []
+    (new-card {:subtitle "dodaj pasażera"})))
+
+(defn list-of-passangers []
+  (let []
+    (new-card {:title "Kto już w aucie"})))
 
 (defn home-panel []
   (let [name (re-frame/subscribe [::subs/name])]
 
      ;[:div [:h1 (str "Hello from " @name ". This is the Home Page.")]
-     [ui/mui-theme-provider {:mui-theme (get-mui-theme (aget js/MaterialUIStyles "DarkRawTheme"))}
+     [ui/mui-theme-provider {:mui-theme (get-mui-theme
+                                         ;(aget js/MaterialUIStyles "DarkRawTheme")
+                                         )}
      		[:div
-       		[ui/app-bar {:title "Razem Na Stok"
-                     	 :showMenuIconButton false
-                     	 :titleStyle {:color (color :lime200)
-                                    :textAlign :center
-                        }}]
-       		[ui/paper {:style {:height "200"
-                             :width "100%"
-  													 :margin-top "10"
-                             :textAlign "center"
-                             :display "inline-block"
-                            }
-                     :zDepth "3"}]
-        		;[:a {:href "#/about"} "go to About Page"]
+       		[app-bar]
+       		[jumbotron]
+					[car-card]
+       		[new-passanger-card]
+       		[list-of-passangers]
+        	[:a {:href "#/about"} "go to About Page"]
 
 
         ]
