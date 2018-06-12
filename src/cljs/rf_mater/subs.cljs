7de1d91e08c9ac60ae3da5ea1acded14ec460b9c
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2018-06-12 09:01:55 +0200

    Move to shadow-cljs

--- src/cljs/rf_mater/subs.cljs
+++ src/cljs/rf_mater/subs.cljs
@@ -1,24 +1,24 @@
 (ns rf-mater.subs
   (:require [re-frame.core :as re-frame]))
 
-(re-frame/reg-sub
- ::name
- (fn [db]
-   (:name db)))
+;; (re-frame/reg-sub
+;;  ::name
+;;  (fn [db]
+;;    (:name db)))
 
-(re-frame/reg-sub
- ::active-panel
- (fn [db _]
-   (:active-panel db)))
-
-
-(re-frame/reg-sub
- :driver
- (fn [db _]
-   (get-in db [:auto :driver])))
+;; (re-frame/reg-sub
+;;  ::active-panel
+;;  (fn [db _]
+;;    (:active-panel db)))
 
 
-(re-frame/reg-sub
- :passengers
- (fn [db _]
-   (get-in db [:auto :passengers])))
+;; (re-frame/reg-sub
+;;  :driver
+;;  (fn [db _]
+;;    (get-in db [:auto :driver])))
+
+
+;; (re-frame/reg-sub
+;;  :passengers
+;;  (fn [db _]
+;;    (get-in db [:auto :passengers])))
