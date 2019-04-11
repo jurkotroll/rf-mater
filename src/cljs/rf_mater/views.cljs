5aabc932797cba84f35c7880681c0720958a6a10
jurkotroll <jurko.bossetroll@gmail.com>
2019-04-11 14:58:43 +0200

    after pause

--- src/cljs/rf_mater/views.cljs
+++ src/cljs/rf_mater/views.cljs
@@ -284,50 +284,7 @@
        [info-block-about] [info-block-divider]
        [info-block-contact-person] [info-block-divider]
        [info-block-auto] [info-block-divider]
-       [info-block-passengers-table]]]]]]); [:> Typography {:component "p"}
-     ;      "This impressive paella is a perfect party dish and a fun meal to cook together
-     ;       with your guests. Add 1 cup of frozen peas along with the mussels, if you like."]
-     ; ]
-     ; [:> CardActions {:style (:actions styles)
-     ;                  :disable-action-spacing false
-     ;                  }
-     ;    [:> IconButton {:aria-label "Add to favorites"}
-     ;      [:> FavoriteIcon]]
-     ;    [:> IconButton {:aria-label "Share"}
-     ;      [:> ShareIcon]]
-     ;    [:> IconButton {:aria-label "Show more"
-     ;                    :style (:expand styles)
-     ;                    ;:on-click
-     ;                    ;:aria-expanded
-     ;                    }
-     ;      [:> ExpandMoreIcon]]]
-     ; [:> Collapse {:in true}
-     ;   [:> CardContent
-     ;     [:> Typography {;:paragraph ""
-     ;                     :variant "body2"}
-     ;       "Method:"]
-     ;     [:> Typography {;:paragraph true
-     ;                     }
-     ;       "Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside
-     ;         for 10 minutes."]
-     ;     [:> Typography {;:paragraph true
-     ;                     }
-     ;       "Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-     ;            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-     ;            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-     ;            chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-     ;            salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-     ;            minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil."]
-     ;     [:> Typography {;:paragraph true
-     ;                     }
-     ;       "Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-     ;            cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-     ;            Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-     ;            the rice, and cook again without stirring, until mussels have opened and rice is
-     ;            just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)"]
-     ;     [:> Typography
-     ;       "Set aside off of the heat to let rest for 10 minutes, and then serve."]
-     ;     ]
+       [info-block-passengers-table]]]]]]); 
 (defn new-user []
  (re-frame/dispatch [::events/add-new-user "user017"]))
 
